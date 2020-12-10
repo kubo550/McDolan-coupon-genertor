0794c2a64603d40a0366ffa5d06a3e2680f49b09
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-10 20:18:30 +0100

    Uploade Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home, Type, Date, Result } from "./components";
+import { getUniqeCode } from "./utils/index";
+import { AnimatePresence } from "framer-motion";
+export type couponType = "hamburger" | "cheeseburger" | "lody" | "frytki";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface couponProps {
+  type: couponType | "";
+  date: string;
+  code: string;
 }
+
+const App = () => {
+  const [coupon, setCoupon] = useState<couponProps>({
+    type: "",
+    date: "",
+    code: getUniqeCode(),
+  });
+
+  const setCouponType = (type: couponType) => setCoupon(prev => ({ ...prev, type }));
+  const setCouponDate = (date: string) => setCoupon(prev => ({ ...prev, date }));
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/step1'>
+          <Type type={coupon.type} setCouponType={setCouponType} />
+        </Route>
+        <Route path='/step2'>
+          <Date date={coupon.date} setCouponDate={setCouponDate} />
+        </Route>
+        <Route path='/result'>
+          <Result coupon={coupon} />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
