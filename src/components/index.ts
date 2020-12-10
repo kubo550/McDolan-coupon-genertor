4eac1e112c849e04e130a71771a4f1795b701be7
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-10 20:19:44 +0100

    upload files

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { Home } from "./Home/Home"
+export { Type } from "./Type/Type"
+export { Date } from "./Date/Date"
+export { Result } from "./Result/Result"
---
+++ src/img/index.ts
@@ -0,0 +1,9 @@
+import hamburger from "./hamburger.png"
+import frytki from "./frytki.png"
+import lody from "./lody.png"
+import cheeseburger from "./cheeseburger.png"
+
+export const imageTypes = {
+    hamburger, frytki, lody, cheeseburger
+}
+
