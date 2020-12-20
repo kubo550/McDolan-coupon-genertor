5bbd51c34e394003c876b05e1910466031f55b6a
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-20 23:31:21 +0100

    Add React Router Dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./styles/global.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
